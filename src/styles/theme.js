046f0b03a110e989d1b70ca3a0a03975e9ce096f
Jiel Selmani <jiel.selmani@gmail.com>
2021-08-27 14:32:43 -0400

    Started the theming process to complete the application

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const ChakradoroTheme = extendTheme({
+    colors: {
+        primary: '#00CFBB',
+        backgroundDark: '#344B47',
+        backgroundLight: '#96B1AC',
+        textLight: '#3887D5',
+        textDark: '#7ABCFF'
+    },
+    components: {}
+
+});
